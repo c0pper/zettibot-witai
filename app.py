e1be414238dc7df116ee0f6cef90f94b431f5fc2
Simo <93simonster@gmail.com>
2021-08-25 21:01:52 +0200

    2nd

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import random
 import logging
 from telegram import Update
 from telegram.ext import Updater, Dispatcher, CommandHandler, MessageHandler, Filters, CallbackContext
@@ -10,24 +11,46 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+
 HEROKU_URL = "https://zettibot-witai.herokuapp.com/"
-PORT = int(os.environ.get('PORT','8433'))
+PORT = int(os.environ.get('PORT', '8433'))
 TELE_TOKEN = "919532199:AAFU7pBcj_L310q3Ud6QkKw2K6KFo9OvaN0"
 AI_TOKEN = "Q7EA3FPHULCOYH5DVZU3Y2PY43DKKOXW"
 
 
-#Define Command Handlers
+# Assets
+
+picfile = 'pics.txt'
+quotefile = 'quotes.txt'
+audiofile = 'audio.txt'
+
+with open(picfile) as p, open(quotefile) as q, open(audiofile) as a:
+    plines = p.readlines()
+    qlines = q.readlines()
+    alines = a.readlines()
+
+
+# Define Command Handlers
 def start(update: Update, context: CallbackContext):
-   """Handler for /start command"""
-   update.message.reply_text('Hi, how are you')
-def helpCommand(update: Update, context: CallbackContext):
-   """Handler for /help command"""
-   update.message.reply_text('Help!')
+    """Handler for /start command"""
+    update.message.reply_text('Hi, how are you')
+
+
 def userText(update: Update, context: CallbackContext):
-   """Function to reply to user text"""
-   ai = Wit(access_token=AI_TOKEN)
-   resp = ai.message(update.message.text)
-   update.message.reply_text(str(resp['intents'][0]['name']))
+    """Function to reply to user text"""
+    ai = Wit(access_token=AI_TOKEN)
+    resp = ai.message(update.message.text)
+    update.message.reply_text(str(resp['intents'][0]['name']))
+
+
+def send_audio(update: Update, context: CallbackContext) -> None:
+    audio = random.choice(alines)
+    update.message.reply_voice(audio)
+
+
+def send_rand_photo(update: Update, context: CallbackContext) -> None:
+    photo = random.choice(plines)
+    update.message.reply_photo(photo, random.choice(qlines))
 
 
 def main():
@@ -39,9 +62,11 @@ def main():
 
     # registering commands
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", helpCommand))
+    dp.add_handler(CommandHandler("audio", send_audio))
+    dp.add_handler(CommandHandler("pic", send_rand_photo))
     # registering Message Handler to reply to user messages
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, userText))
+
     # starting the bot
     updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
